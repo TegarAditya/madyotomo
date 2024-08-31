b25329f67c3e4e9296d0347ffe9e7f243f83cdf7
Tegar Aditya <tegaraditya53@gmail.com>
2024-08-31 12:21:22 +0700

    feat: add semester selector

--- app/Filament/Admin/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Admin/Resources/OrderResource/Pages/ListOrders.php
@@ -4,6 +4,7 @@ namespace App\Filament\Admin\Resources\OrderResource\Pages;
 
 use App\Filament\Admin\Resources\OrderResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Builder;
@@ -22,5 +23,26 @@ class ListOrders extends ListRecords
     protected function paginateTableQuery(Builder $query): Paginator
     {
         return $query->fastPaginate(($this->getTableRecordsPerPage() === 'all') ? $query->count() : $this->getTableRecordsPerPage());
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = [
+            null => Tab::make('All'),
+        ];
+
+        $semesters = \App\Models\Semester::all()->pluck('name', 'id')->toArray();
+
+        foreach ($semesters as $id => $name) {
+            $tabs[$id] = Tab::make($name)->query(function ($query) use ($id) {
+                return $query->whereHas('order_products', function ($query) use ($id) {
+                    $query->whereHas('product', function ($query) use ($id) {
+                        $query->where('semester_id', $id);
+                    });
+                });
+            });
+        }
+
+        return $tabs;
     }
 }
