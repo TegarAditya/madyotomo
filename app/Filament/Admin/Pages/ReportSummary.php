db60a3ff15031c9e5d9a10f5a56f2316efe3cd59
Tegar Aditya <tegaraditya53@gmail.com>
2024-10-04 08:58:06 +0700

    Refactor ReportSummary page and view, SpkProduct model, ProductReport model, and Product model

--- app/Filament/Admin/Pages/ReportSummary.php
+++ app/Filament/Admin/Pages/ReportSummary.php
@@ -2,14 +2,17 @@
 
 namespace App\Filament\Admin\Pages;
 
+use App\Models\OrderProduct;
 use App\Models\ProductReport;
 use Filament\Pages\Page;
 use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
+use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Filters\QueryBuilder\Constraints\DateConstraint;
 use Filament\Tables\Table;
+use Illuminate\Support\HtmlString;
 use Malzariey\FilamentDaterangepickerFilter\Filters\DateRangeFilter;
 
 class ReportSummary extends Page implements HasTable
@@ -33,23 +36,34 @@ class ReportSummary extends Page implements HasTable
             ->columns([
                 Tables\Columns\TextColumn::make('spk.order.proof_number')
                     ->label('No. SPK')
+                    ->sortable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('spk.order.name')
+                    ->sortable()
                     ->label('Order'),
-                Tables\Columns\TextColumn::make('spkProduct.products')
-                    ->label('Produk'),
-                Tables\Columns\TextColumn::make('date')
-                    ->label('Tanggal Produksi')
-                    ->date(),
+                Tables\Columns\TextColumn::make('products')
+                    ->label('Produk')
+                    ->weight(FontWeight::Bold)
+                    ->default(fn(ProductReport $record) => new HtmlString($this->getProductsName($record))),
+                Tables\Columns\TextColumn::make('machine.name')
+                    ->label('Mesin')
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('success_count')
                     ->label('Hasil Baik')
                     ->weight(FontWeight::Bold)
+                    ->summarize(Sum::make()->label('Total'))
+                    ->sortable()
                     ->numeric(),
-                Tables\Columns\TextColumn::make('machine.name')
-                    ->label('Mesin'),
+                Tables\Columns\TextColumn::make('date')
+                    ->label('Tanggal Produksi')
+                    ->sortable()
+                    ->date(),
             ])
             ->filters([
                 DateRangeFilter::make('date'),
+                Tables\Filters\SelectFilter::make('machine_id')
+                    ->label('Mesin')
+                    ->options(fn() => \App\Models\Machine::pluck('name', 'id')->toArray()),
             ])
             ->actions([
                 // ...
@@ -58,5 +72,12 @@ class ReportSummary extends Page implements HasTable
                 // ...
             ])
             ->defaultSort('created_at', 'desc');
+    }
+
+    protected function getProductsName(ProductReport $record): string
+    {
+        return collect($record->spkProduct->order_products)->map(function ($product) {
+            return OrderProduct::find($product)->product->short_name;
+        })->implode('<hr>');
     }
 }
